c63cd1997aee85f93475af14bd49590531469973
Zawalid <walid.zakan@gmail.com>
2025-01-20 18:11:07 +0100

    FInal commit for the damn vercel

--- src/lib/utils/emailParser.ts
+++ src/lib/utils/emailParser.ts
@@ -1,10 +1,12 @@
-import { simpleParser } from 'mailparser';
+import { simpleParser } from "mailparser";
 
-export async function parseEmail(rawEmail: string): Promise<ParsedEmail> {
-  const decodedEmail = Buffer.from(rawEmail, 'base64').toString('utf-8');
+export async function parseEmail(rawEmail: string) {
+  const decodedEmail = Buffer.from(rawEmail, "base64").toString("utf-8");
   const parsed = await simpleParser(decodedEmail);
 
   const { from, subject } = parsed;
+
+  if (!from) return null;
 
   return {
     from: from.value,
