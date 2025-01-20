66b6b5b73b7b4a6fd0af3b77fac090c7f4b759bf
Zawalid <walid.zakan@gmail.com>
2025-01-20 17:49:41 +0100

    Just for deployment

--- src/lib/api/emails.ts
+++ src/lib/api/emails.ts
@@ -6,7 +6,7 @@ interface Email {
   id: string;
 }
 
-export async function getEmail(client: OAuth2Client, email: Email): Promise<ParsedEmail> {
+export async function getEmail(client: OAuth2Client, email: Email) {
   const res = await axios.get(`https://www.googleapis.com/gmail/v1/users/me/messages/${email.id}`, {
     headers: {
       Authorization: `Bearer ${client.credentials.access_token}`,
@@ -19,12 +19,7 @@ export async function getEmail(client: OAuth2Client, email: Email): Promise<Pars
   const decodedEmail = Buffer.from(rawEmail, "base64").toString("utf-8");
   const parsed = await simpleParser(decodedEmail);
 
-  const { from, subject } = parsed;
+  // const { from, subject } = parsed;
 
   console.log(parsed);
-
-  return {
-    from: from.value,
-    subject,
-  };
 }
